43ecd0c1795775e67a0593c33fbddf7928d097ac
pevehdev <pauloht2020@gmail.com>
2022-10-07 17:48:25 -0300

    Adicionado links do linkedin restantes e separado o formulario em 2 blocos

--- js/validarForm.js
+++ js/validarForm.js
@@ -1,1 +1,32 @@
+const email = document.getElementById('email')
+const senha = document.getElementById('senha1')
+const form = document.getElementById("formulario")
 
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    checkInput();
+})
+
+
+function checkInput(){
+   const emailValue = email.value
+   const senhaValue = senha.value
+   console.log(emailValue)
+   console.log(senhaValue)
+}
+
+
+if(emailValue ===''){
+    
+
+    setErrorFor(emailValue, 'Email não pode estar vazio');
+}else{
+    setSuccessFor(emailValue);
+}
+function setErrorFor(input, message){
+ const campo = input.parentElement;
+ const small = campo.querySelector('small');
+
+ small.innerText = message;
+ campo.className = 'campo'
+}
